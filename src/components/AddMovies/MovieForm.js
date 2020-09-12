b50554889f3ce4f1905a6f8d553f9311927e666e
Mary Ruth Diolas <mdiosgalla@gmail.com>
2020-09-12 23:21:01 +0800

    added create movie functionality

--- src/components/AddMovies/MovieForm.js
+++ src/components/AddMovies/MovieForm.js
@@ -1,25 +1,91 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import MovieInput from './../inputs/MovieInput';
+import {Redirect} from 'react-router-dom';
 
 const MovieForm = (props) => {
+
+    const [redirect, setRedirect] = useState({
+        latestId: "",
+        success: false
+    });
+
+    const [movie, setMovie] = useState({
+        name: "",
+        price: "",
+        description: "",
+        image: {}
+    });
+
+    if (redirect.success) {
+        return <Redirect to={`/movies/${redirect.latestId}`} />
+    }
+
+    const handleChange = e => {
+        setMovie({
+            ...movie,
+            [e.target.name]: e.target.value
+        });
+    };
+
+
+    const handleChangeFile = e => {
+        setMovie({
+            ...movie,
+            image: e.target.files[0]
+        });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        let formData = new FormData();
+        formData.append('name', movie.name)
+        formData.append('price', movie.price)
+        formData.append('description', movie.description)
+        formData.append('image', movie.image)
+
+        fetch('https://booking-movie-backend.herokuapp.com/movies', {
+            method: "post",
+            body: formData,
+            headers: {
+                "Authorization": `Bearer ${localStorage['appState']}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data._id) {
+                    setRedirect({
+                        latestId: data._id,
+                        success: true
+                    })
+                }
+                console.log(data)
+            })
+    }
+
+
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <MovieInput
                 name="name"
                 displayName="Movie Name:"
                 type="text"
+                handleChange={handleChange}
             />
 
             <MovieInput
                 name="price"
                 displayName="Movie Price:"
                 type="number"
+                handleChange={handleChange}
             />
             
             <MovieInput
                 name="image"
                 displayName="Movie Image:"
                 type="file"
+                handleChange={handleChangeFile}
             />
 
     	
@@ -31,7 +97,8 @@ const MovieForm = (props) => {
     	name="description" 
     	id="description" 
     	cols="30" 
-    	rows="10"></textarea>
+        rows="10"
+        onChange={handleChange}></textarea>
     	<br />
             <button className="submit btn-primary d-block btn-sm">Add Movie</button>
     </form>
