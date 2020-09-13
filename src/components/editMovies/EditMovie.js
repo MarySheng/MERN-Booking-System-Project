d6d8ee3ef215f6d95e7401d3572444ac5c9ab0e9
Mary Ruth Diolas <mdiosgalla@gmail.com>
2020-09-13 12:51:07 +0800

    added edit functionality

--- src/components/editMovies/EditMovie.js
+++ src/components/editMovies/EditMovie.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import EditMovieForm from './EditMovieForm';
+import {useParams} from 'react-router-dom';
 
-const EditMovie = () => {
+const EditMovie = (props) => {
+
+    const { id } = useParams();
+
+    const [movie, setMovie] = useState({
+        name: "",
+        image: "",
+        price: "",
+        description: ""
+    });
+
+    useEffect(() => {
+        fetch(`https://booking-movie-backend.herokuapp.com/movies/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setMovie(data)
+            })
+    },[])
+
+
     return (
         <div className="ui container-fluid">
             <div className="row">
@@ -12,7 +32,7 @@ const EditMovie = () => {
             <div className="row">
                 <div className="col-12 col-md-6 mx-auto">
                     
-                    <EditMovieForm/>
+                    <EditMovieForm movie={movie}/>
                     
                  </div> 
             </div>
--- src/components/editMovies/EditMovieForm.js
+++ src/components/editMovies/EditMovieForm.js
@@ -1,25 +1,99 @@
-import React from 'react';
-import MovieInput from './../inputs/MovieInput'
+import React, {useState, useEffect} from 'react';
+import MovieInput from './../inputs/MovieInput';
+import {Redirect} from 'react-router-dom';
 
-const EditMovieForm = () => {
+const EditMovieForm = (props) => {
+
+    const [movie, setmovie] = useState({});
+
+    const [redirect, setRedirect] = useState(false);
+
+
+    const handleChange = e => {
+        setmovie({
+            ...movie,
+            [e.target.name]: e.target.value
+        });
+    };
+
+
+   const handleChangeFile = e => {
+        setmovie({
+            ...movie,
+            image: e.target.files[0]
+        });
+    };
+
+
+   const handleSubmit = e => {
+        e.preventDefault();
+
+        let formData = new FormData();
+        formData.append('name', movie.name);
+        formData.append('price', movie.price);
+        formData.append('description', movie.description);
+
+        if (movie.image) {
+            formData.append('image', movie.image);
+        }
+
+        fetch(`https://booking-movie-backend.herokuapp.com/movies/${props.movie._id}`, {
+            method: "put",
+            headers: {
+                "Authorization" : `Bearer ${localStorage['appState']}`
+            },
+            body: formData
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setRedirect(true)
+            })
+    }
+
+
+    useEffect(() => {
+        setmovie({
+            ...movie,
+            ...props.movie,
+            image: ""
+        })
+    }, [props.movie])
+
+    if (redirect) {
+        return <Redirect to={`/movies/${props.movie._id}`} />
+    }
+
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <MovieInput
                 name="name"
                 displayName="Movie Name:"
                 type="text"
+                value={movie.name}
+                required
+                autocomplete="name"
+                autofocus
+                handleChange={handleChange}
             />
 
             <MovieInput
                 name="price"
                 displayName="Movie Price:"
                 type="number"
+                required
+                autofocus
+                handleChange={handleChange}
             />
             
             <MovieInput
                 name="image"
                 displayName="Movie Image:"
                 type="file"
+                required
+                autofocus
+                handleChange={handleChangeFile}
             />
 
     	
@@ -31,7 +105,9 @@ const EditMovieForm = () => {
     	name="description" 
     	id="description" 
     	cols="30" 
-    	rows="10"></textarea>
+        rows="10"
+        value={movie.description}
+        onChange={handleChange}></textarea>
     	<br />
             <button className="submit btn-primary d-block btn-sm">Edit Movie</button>
     </form>
